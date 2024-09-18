9a59dcf9a7ef5e6f3e186d68dcfc7848e4ca5d66
Potseluiko Vitalii <vetaliy063@gmail.com>
2024-09-18 14:25:07 +0300

    added mapstruct tool

--- src/main/java/com/bookmanager/app/mapper/BookMapper.java
+++ src/main/java/com/bookmanager/app/mapper/BookMapper.java
@@ -1,32 +1,14 @@
 package com.bookmanager.app.mapper;
 
+import com.bookmanager.app.config.MapperConfig;
 import com.bookmanager.app.dto.BookRequestDto;
 import com.bookmanager.app.dto.BookResponseDto;
 import com.bookmanager.app.model.Book;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class BookMapper implements Mapper<BookRequestDto, Book, BookResponseDto> {
-    @Override
-    public Book toModel(BookRequestDto requestDto) {
-        Book book = new Book();
-        book.setTitle(requestDto.getTitle());
-        book.setAuthor(requestDto.getAuthor());
-        book.setPublicationYear(requestDto.getPublicationYear());
-        book.setGenre(requestDto.getGenre());
-        book.setIsbn(requestDto.getIsbn());
-        return book;
-    }
+@Mapper(config = MapperConfig.class)
+public interface BookMapper {
+    Book toModel(BookRequestDto requestDto);
 
-    @Override
-    public BookResponseDto toDto(Book model) {
-        BookResponseDto responseDto = new BookResponseDto();
-        responseDto.setId(model.getId());
-        responseDto.setAuthor(model.getAuthor());
-        responseDto.setTitle(model.getTitle());
-        responseDto.setIsbn(model.getIsbn());
-        responseDto.setGenre(model.getGenre());
-        responseDto.setPublicationYear(model.getPublicationYear());
-        return responseDto;
-    }
+    BookResponseDto toDto(Book model);
 }
--- src/main/java/com/bookmanager/app/mapper/Mapper.java
+++
@@ -1,7 +0,0 @@
-package com.bookmanager.app.mapper;
-
-public interface Mapper<R, M, D> {
-    M toModel(R requestDto);
-
-    D toDto(M model);
-}
